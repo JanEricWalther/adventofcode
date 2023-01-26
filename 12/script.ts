20f0c2438e1b9e01007dcda85d8ec0b32641ee68
Jan Walther <mail@janericwalther.de>
2023-01-26 16:37:21 +0100

    cleanup

--- 12/script.ts
+++ 12/script.ts
@@ -1,86 +1,87 @@
+class Point {
+  #x: number;
+  #y: number;
+
+  constructor(x: number, y: number) {
+    this.#x = x;
+    this.#y = y;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  toJSON() {
+    return {
+      x: this.#x,
+      y: this.#y,
+    };
+  }
+}
+
 const input = Deno.readTextFileSync(Deno.args[0]).trim();
 
 console.log("Fewest Steps:", solve(parse(input)));
-
-function parse(input: string) {
-  // map[y][x]
-  // const map: Point[][] = [];
-  const map = [];
-
-  for (const line of input.split("\n")) {
-    //   const row: Point[] = [];
-    //   for (const char of line.trim().split("")) {
-    //     const val = char.charCodeAt(0);
-    //     row.push(new Point(val));
-    //   }
-    map.push(line.trim().split(""));
-  }
-  return map;
-}
 
 function solve(input: string[][]) {
   const map: number[][] = Array(input.length).fill(null).map((_) =>
     Array(input[0].length).fill(false)
   );
-  let S;
-  let E;
+  let S: Point;
+  let E: Point;
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
       const cell = input[y][x];
       if (cell === "S") {
-        S = { x, y };
-        // Your current position (`S`) has elevation `a`
+        S = new Point(x, y);
+        // Starting position S has elevation a
         input[y][x] = "a";
       } else if (cell === "E") {
-        E = { x, y };
-        // And the location that should get the best signal (`E`) has elevation `z`
+        E = new Point(x, y);
+        // Destination E has elevation z
         input[y][x] = "z";
       }
       // While we are looping, re-encode chars to ints, to make our elevation comparisons easier
-
       map[y][x] = input[y][x].charCodeAt(0);
     }
   }
-  if (!S || !E) {
-    throw new Error("invalid input");
-  }
-
-  const path = getShortestPath(S.x, S.y, E.x, E.y);
+  const path = getShortestPath(S!, E!);
   return path.length;
 
-  function toId(x: number, y: number) {
-    return `${x},${y}`;
-  }
-
-  function getNeighbors(x: number, y: number) {
+  function getNeighbors(p: Point) {
     return [
-      { x: x, y: y - 1 },
-      { x: x - 1, y: y },
-      { x: x + 1, y: y },
-      { x: x, y: y + 1 },
+      new Point(p.x, p.y - 1),
+      new Point(p.x - 1, p.y),
+      new Point(p.x + 1, p.y),
+      new Point(p.x, p.y + 1),
     ].filter((coord) => typeof map[coord.y]?.[coord.x] !== "undefined");
   }
 
-  function buildFrontier(from_x: number, from_y: number) {
-    const frontier = [];
-    frontier.push({ x: from_x, y: from_y });
+  function buildFrontier(from: Point) {
+    const frontier: Point[] = [];
+    frontier.push(from);
 
-    const came_from = new Map();
-    came_from.set(toId(from_x, from_y), null);
+    const came_from: Map<string, string> = new Map();
+    came_from.set(JSON.stringify(from), "start");
+
     while (frontier.length > 0) {
-      const current = frontier.shift() ?? { x: 0, y: 0 };
+      const current = frontier.shift() ?? new Point(0, 0);
       const current_val = map[current.y][current.x];
 
-      const neighbors = getNeighbors(current.x, current.y);
+      const neighbors = getNeighbors(current);
       for (const next of neighbors) {
         const next_cell = map[next.y][next.x];
-        const next_id = toId(next.x, next.y);
+        const next_id = JSON.stringify(next);
 
         if (next_cell - current_val > 1 || came_from.has(next_id)) {
           continue;
         }
-        // Coord is walkable
-        const current_id = toId(current.x, current.y);
+
+        const current_id = JSON.stringify(current);
         frontier.push(next);
         came_from.set(next_id, current_id);
       }
@@ -88,44 +89,32 @@ function solve(input: string[][]) {
     return came_from;
   }
 
-  function getShortestPath(
-    from_x: number,
-    from_y: number,
-    to_x: number,
-    to_y: number,
-  ) {
-    const from_id = toId(from_x, from_y);
-    const to_id = toId(to_x, to_y);
-    const came_from = buildFrontier(from_x, from_y);
-    let current = to_id;
+  function getShortestPath(from: Point, to: Point) {
+    const came_from = buildFrontier(from);
+    let current = JSON.stringify(to);
+    const path: string[] = [];
 
-    const path = [];
-    while (current !== undefined && current !== from_id) {
+    while (current !== JSON.stringify(from)) {
       path.push(current);
-      current = came_from.get(current);
+      if (!(current = came_from.get(current)!)) {
+        break;
+      }
     }
 
-    // An undefined `current` means it wasn't possible to have a path `from` -> `to`, return an empty path
+    // An undefined current means no possible path was found
     if (current === undefined) {
       return [];
     }
 
-    // Finally, put `from` first, and `to` last
-    // Note our path won't include the `from` position
+    // path won't include the from Point
     return path.reverse();
   }
 }
 
-class Point {
-  #height: number;
-  possibleNeighbors: Point[];
-
-  constructor(height: number) {
-    this.#height = height;
-    this.possibleNeighbors = [];
+function parse(input: string) {
+  const map = [];
+  for (const line of input.split("\n")) {
+    map.push(line.trim().split(""));
   }
-
-  get height() {
-    return this.#height;
-  }
+  return map;
 }
